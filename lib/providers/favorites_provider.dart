0fbb812e9f02f07b3facc081d3d36e4020b4e2fd
Jay Zisch <jz709u@gmail.com>
2024-12-27 16:16:13 -0700

    new riverpod providers

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleMealFavoriteStates(Meal meal) {
+    if (state.contains(meal)) {
+      state = state.where((_meal) {
+        return _meal.id == meal.id;
+      }).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
